4270d822980eaa9291d46565ab033425af7180ee
mystic <215104920@qq.com>
2018-11-25 19:46:43 +0800

    [config server] learning

---
+++ config_client/src/main/java/com/example/config/client/ConfigClientApplication.java
@@ -0,0 +1,16 @@
+package com.example.config.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: mystic
+ * @date: 2018/11/25 19:43
+ * Description:
+ */
+@SpringBootApplication
+public class ConfigClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+}
